f5d874ca0b49abdeff2c4caf1deaef389e89dfea
BredgeFox <47324056+BredgeFox@users.noreply.github.com>
2019-10-02 14:27:19 +0200

    fus quen

--- Program.cs
+++ Program.cs
@@ -16,9 +16,11 @@ namespace PPE3_Leviathan
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            ControleurMission1.init();
-            controleurMission3.init();
-            Application.Run(new fGestionRembFrais());
+            //ControleurMission1.init();
+            controleurMission2.init();
+            //controleurMission3.init();
+
+            Application.Run(new Form2());
             // test
         }
     }
